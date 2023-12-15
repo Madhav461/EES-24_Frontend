355a2280c699ea22b9a8601709718570dcec11e4
ashutuptiwari <ashutosh.tiwari.cd.ece22@itbhu.ac.in>
2023-12-16 00:41:28 +0530

    add svg files

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -3,27 +3,41 @@ import "./About.css";
 
 export const About = () => {
   return (
-    <div class="center">
-      <div className="div">
-        <div class="upper">
-          <div className="h">About Us</div>
-          <div class='svg'>
-            <img src="\Frame (1).svg" class='img'></img>
+      
+      <div id="container">
+        <div class="svg4">
+        <img src="Group 12.svg"></img>
+         </div>
+        <div class="center">
+          <div class="column1">
+            <div class="svg3">
+              <img src="\Vector 22.svg"></img>
+            </div>
+            <div class="svg2">
+              <img src="\Group 7.svg" class="img2"></img>
+            </div>
+          </div>
+          <div className="div">
+            <div class="upper">
+              <div className="h">About Us</div>
+              <div class="svg1">
+                <img src="\Frame (1).svg" class="img1"></img>
+              </div>
+            </div>
+            <p className="content">
+              <span className="text-wrapper">
+                vitae sapien pellentesque habitant morbi tristique senectus et
+                netus et malesuada fames ac turpis egestas sed tempus urna et
+                pharetra pharetra massa massa ultricies mi quis hendrerit dolor
+                magna eget est lorem ipsum dolor sit amet consectetur adipiscing
+                elit pellentesque habitant morbi tristique senectus et netus et
+                malesuada fames ac turpis egestas integer eget aliquet nibh
+                praesent tristique.
+              </span>
+            </p>
           </div>
         </div>
-        <p className="content">
-          <span className="text-wrapper">
-            vitae sapien pellentesque habitant morbi tristique senectus et netus
-            et malesuada fames ac turpis egestas sed tempus urna et pharetra
-            pharetra massa massa ultricies mi quis hendrerit dolor magna eget
-            est lorem ipsum dolor sit amet consectetur adipiscing elit
-            pellentesque habitant morbi tristique senectus et netus et malesuada
-            fames ac turpis egestas integer eget aliquet nibh praesent
-            tristique.
-          </span>
-        </p>
       </div>
-    </div>
   );
 };
 
