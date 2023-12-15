a7fd376e7eb8ffd6255296de3e31fb89d20e560d
say-nn-hahaa <sayanhamondalxii30@gmail.com>
2023-12-15 19:06:27 +0530

    ADDED ABOUT-US

--- frontend/src/components/landingPage.js
+++ frontend/src/components/landingPage.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Background from "./background";
+import About from './About'; 
+
 const LandingPage = () => {
   return (
     <div className="text-white">
       <Background />
-      <h1 className="text-white text-center text-2xl">EES hehe</h1>
+      <h1 className="text-white text-center text-2xl">EES hehe</h1> 
+      <About />
     </div>
   );
 };
