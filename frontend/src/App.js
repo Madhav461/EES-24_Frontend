ff86c48c3ebc5a9329990cd31e777ef4552df1e5
amaan1203 <127188069+amaan1203@users.noreply.github.com>
2023-12-15 15:59:35 +0530

    added Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/landingPage";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<LandingPage />} />
       </Routes>
+      <Footer></Footer>
     </Router>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: "#111115",
+        "pitchdeck-slide-background": "#fff",
+        whitesmoke: "#f8f8f8",
+        gainsboro: {
+          "100": "#d9d9d9",
+          "200": "rgba(217, 217, 217, 0.2)",
+        },
+        deepskyblue: "#16b9ff",
+        turquoise: "#25f2da",
+      },
+      spacing: {},
+      fontFamily: {
+        "bruno-ace-sc": "'Bruno Ace SC'",
+        goldman: "Goldman",
+        "libre-barcode-39": "'Libre Barcode 39'",
+        ligconsolata: "Ligconsolata",
+        audiowide: "Audiowide",
+        michroma: "Michroma",
+      },
+    },
+    fontSize: {
+      mini: "15px",
+      "45xl": "64px",
+      "77xl": "96px",
+      xl: "20px",
+      "17xl": "36px",
+      "3xl-7": "22.7px",
+      "5xl": "24px",
+      base: "16px",
+      "13xl": "32px",
+      "2xl": "21px",
+      "14xl": "33px",
+      inherit: "inherit",
+    },
   },
-  plugins: [],
-}
+  corePlugins: {
+    preflight: false,
+  },
+};
