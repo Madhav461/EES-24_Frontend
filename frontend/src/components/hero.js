d2d50a2197c66bc016b493e243e4b583a3bc571c
santosh-k22 <kothapalli.santosh.ece22@itbhu.ac.in>
2023-12-15 12:15:33 +0530

    completed hero section

---
+++ frontend/src/components/hero.js
@@ -0,0 +1,14 @@
+// import "./navHero.css"
+import "./hero.css"
+
+function Hero() {
+  return (
+    <div className="container">
+      <div className="p1"> <div className="text">Currents of Creativity, Voltage of Vision: Electrify the Future.</div></div>
+      <img className="logo" src="ees_logo_1.svg" alt="EES Logo" />
+      <div className="p2"><div>Greetings. Electronics Engineers. To the fest beyond imagination</div></div>
+    </div>
+  );
+}
+
+export default Hero;
