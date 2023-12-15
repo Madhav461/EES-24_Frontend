69edecdcec45b29f144af66ef2e08b49ce76036a
ashutuptiwari <ashutosh.tiwari.cd.ece22@itbhu.ac.in>
2023-12-15 21:53:33 +0530

    improve about us, finishing work remains

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,27 +1,21 @@
 import React from "react";
-import './About.css';
-
+import "./About.css";
 
 export const About = () => {
   return (
-    <div className="box">
-      <div className="group">
-        <div className="overlap">
-          <div className="overlap-group-wrapper">
-            <div className="overlap-group">
-              <div className="div">
-              <div className="h">About Us</div>
-                <p className="content">
-                  <span className="text-wrapper">
-                    vitae sapien pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
-                    egestas sed tempus urna et pharetra pharetra massa massa ultricies mi quis hendrerit dolor magna eget est lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant morbi tristique
-                    senectus et netus et malesuada fames ac turpis egestas integer eget aliquet nibh praesent tristique.
-                  </span>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+    <div class="center">
+      <div className="div">
+        <div className="h">About Us</div>
+        <p className="content">
+          <span className="text-wrapper">
+            vitae sapien pellentesque habitant morbi tristique senectus et netus
+            et malesuada fames ac turpis egestas sed tempus urna et pharetra
+            pharetra massa massa ultricies mi quis hendrerit dolor magna eget est
+            lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque
+            habitant morbi tristique senectus et netus et malesuada fames ac
+            turpis egestas integer eget aliquet nibh praesent tristique.
+          </span>
+        </p>
       </div>
     </div>
   );
--- frontend/src/components/landingPage.js
+++ frontend/src/components/landingPage.js
@@ -1,22 +1,16 @@
 import React from "react";
 import Background from "./background";
 
-import About from './About'; 
+import About from "./About";
 
 import Hero from "./hero";
-
 
 const LandingPage = () => {
   return (
     <div className="text-white">
       <Background />
-
-      <h1 className="text-white text-center text-2xl">EES hehe</h1> 
       <About />
-
       <Hero />
-      {/* <h1 className="text-white text-center text-2xl">EES hehe</h1> */}
-
     </div>
   );
 };
