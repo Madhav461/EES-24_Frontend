6212d71124f3bf01c2df1074c76a86a8bd9c25af
ashutuptiwari <ashutosh.tiwari.cd.ece22@itbhu.ac.in>
2023-12-16 13:58:23 +0530

    add glow effect to heading

--- frontend/src/components/landingPage.js
+++ frontend/src/components/landingPage.js
@@ -1,18 +1,16 @@
 import React from "react";
 import Background from "./background";
-
 import About from "./About";
-
 import Hero from "./hero";
 import Navhome from "./navhome";
 
 const LandingPage = () => {
   return (
     <div className="text-white absolute">
-       <Navhome/>
-      <Background /
+      <Background />
+      <Navhome />
+      <Hero />
       <About />
-       <Hero />
     </div>
   );
 };
--- frontend/src/components/navhome.js
+++ frontend/src/components/navhome.js
@@ -1,6 +1,5 @@
-import React from 'react'
-import "./nav.css"
-
+import React from "react";
+import "./nav.css";
 
 const Navhome = () => {
   return (
@@ -28,16 +27,13 @@ const Navhome = () => {
           </div>
         </ul>
         <div className="absolute top-[-9px] left-[calc(50%_-_336.45px)] w-[669.37px] h-[26px]">
-
           <img
             className="w-full h-full object-contain absolute left-[0px] top-[0px] [transform:scale(1.077)]"
             alt=""
             src="/vector-line.svg" //uper white
           />
 
-          <div className="absolute top-[0px] left-[calc(50%_-_334.69px)] w-[669.37px] h-[26px] flex items-center justify-center">
-
-          </div>
+          <div className="absolute top-[0px] left-[calc(50%_-_334.69px)] w-[669.37px] h-[26px] flex items-center justify-center"></div>
         </div>
         <div className="absolute top-[79px] left-[calc(50%_-_1015px)] w-[2030px] h-[26px]">
           <img
@@ -59,16 +55,12 @@ const Navhome = () => {
           src="/ees-logo-fcfbfc-1.svg"
         />
 
-
         {/* button login /signup */}
         <button className="cursor-pointer  [border:none] p-0 bg-[transparent] absolute top-[17px] left-[1149px] w-[116px] h-[38px]">
           <div className="absolute top-[8px] left-[19px] w-[79px] h-6">
             <div className="absolute top-[11px] left-[0px] box-border w-3 h-[13px] border-b-[1px] border-solid border-white border-l-[1px]" />
             <div className="absolute top-[13px] left-[79px] box-border w-3 h-[13px] [transform:_rotate(180deg)] [transform-origin:0_0] border-b-[1px] border-solid border-white border-l-[1px]" />
           </div>
-
-
-
 
           <button className="cursor-pointer glow-animation [border:none] p-0 bg-[transparent] absolute top-[0px] left-[0px] text-base tracking-[0.08em] font-audiowide text-white text-center flex items-center justify-center w-[116px] h-[38px] [text-shadow:1px_0_0_#000,_0_1px_0_#000,_-1px_0_0_#000,_0_-1px_0_#000]">
             Login
@@ -83,11 +75,9 @@ const Navhome = () => {
             Sign Up
           </button>
         </button>
-
       </div>
-
     </div>
   );
 };
 
-export default Navhome
+export default Navhome;
