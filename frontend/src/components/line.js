bd94be7a36a478ebc114daa5a844839abd61374a
Jarvis-013 <124374386+Jarvis-013@users.noreply.github.com>
2023-12-19 12:45:40 +0530

    Update line.js

--- frontend/src/components/line.js
+++ frontend/src/components/line.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 const VerticalLine = () => {
   const lineStyle = {
-    width: '10px',
-    height: '100px', // Adjust the height as needed
-    background: 'white', // Change color as needed
+    height: '2px', // Adjust the height as needed
+    width: '34%', // Full width of the container
+    background: 'white',
+    position: 'absolute',
+    top: '54%', // Adjust the top position to move the line vertically
+    left: '48%', // Align the left side of the line to the left of the container
+    transform: 'translateY(-50%) rotate(90deg)', // Center the line vertically and rotate
+    zIndex: '2',
   };
 
   return (
