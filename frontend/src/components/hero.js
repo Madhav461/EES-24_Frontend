7a63759696ea1bb002cde18305139dd8842d21cb
Madhav461 <ygiridhar.madhav.ece21@itbhu.ac.in>
2023-12-21 16:09:37 +0530

    modified:   frontend/package-lock.json 	modified:   frontend/package.json 	modified:   frontend/src/components/hero.css 	modified:   frontend/src/components/hero.js

--- frontend/src/components/hero.js
+++ frontend/src/components/hero.js
@@ -1,10 +1,15 @@
 // import "./navHero.css"
 import "./hero.css";
 import Icon from "./Icon";
+import Marquee from "react-fast-marquee";
 const Hero = () => {
   return (
+    <div className="h-screen w-screen">
+      <Marquee  className="border-solid  border-teal-300 border-y md:h-[7%] h-[10%] font-bold tracking-widest  glow-animation"><p class="marquee">EVENTS * WORKSHOPS * COMPETETIONS * LECTURES * STALLS * SPORTS * PERFORMENCES * ARTS * EVENTS * WORKSHOPS * COMPETETIONS * LECTURES * STALLS * SPORTS * PERFORMENCES * ARTS *</p></Marquee>
     <div className="container z-0 m-auto">
+      
       <div className="p1">
+      
         <div className="text">
           {/* Keep the content that you want here */}
           Currents of Creativity, Voltage of Vision: Electrify the Future!
@@ -19,6 +24,7 @@ const Hero = () => {
           Greetings. Electronics Engineers. To the fest beyond imagination!
         </div>
       </div>
+    </div>
     </div>
   );
 };
