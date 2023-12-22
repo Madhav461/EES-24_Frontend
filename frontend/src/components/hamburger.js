caca0f3a9bae6907ee1c5b49d6955ed7d9e441a6
satyam-jaiswal-03 <satyamjais.31150@gmail.com>
2023-12-22 15:25:22 +0530

    added hamburger menu

---
+++ frontend/src/components/hamburger.js
@@ -0,0 +1,168 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const COLORS = {
+  primaryDark: "black",
+  primaryLight: "#16b9ff",
+};
+
+const MenuLabel = styled.label`
+//   background-color: ${COLORS.primaryLight};
+//   position: ;
+//   top: -1rem;
+//   right:100vw;
+  left:-1rem;
+//   border-radius: 50%;
+  height: 7rem;
+  width: 7rem;
+  cursor: pointer;
+  z-index: 1000;
+//   box-shadow: 0 1rem 3rem ;
+  text-align: center;
+`;
+
+const NavBackground = styled.div`
+  position: absolute;
+  top: 6.5rem;
+  right: 6.5rem;
+//   background-image:
+    // ${COLORS.primaryLight}
+//   ;
+  background-color:black;
+  height: 100vh;
+  width:100vh;
+  border-radius: 50%;
+  z-index: 600;
+  transform: ${(props) => (props.clicked ? "scale(80)" : "scale(0)")};
+  transition: transform 0.8s;
+`;
+
+const Icon = styled.span`
+  position: relative;
+  background-color: ${(props) => (props.clicked ? "transparent" : "white")};
+  width: 3rem;
+  height: 2px;
+  display: inline-block;
+  margin-top: 3.5rem;
+  transition: all 0.3s;
+  z-index:1000;
+
+  &::before,
+  &::after {
+    content: "";
+    background-color: white;
+    width: 3rem;
+    height: 2px;
+    display: inline-block;
+
+    position: absolute;
+    left: 0;
+    transition: all 0.3s;
+  }
+
+  &::before {
+    top: ${(props) => (props.clicked ? "0" : "-0.8rem")};
+    transform: ${(props) => (props.clicked ? "rotate(135deg)" : "rotate(0)")};
+  }
+
+  &::after {
+    top: ${(props) => (props.clicked ? "0" : "0.8rem")};
+
+    transform: ${(props) => (props.clicked ? "rotate(-135deg)" : "rotate(0)")};
+  }
+
+  ${MenuLabel}:hover &::before {
+    top: ${(props) => (props.clicked ? "0" : "-1rem")};
+  }
+  ${MenuLabel}:hover &::after {
+    top: ${(props) => (props.clicked ? "0" : "1rem")};
+  }
+`;
+
+const Navigation = styled.nav`
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 600;
+  width: ${(props) => (props.clicked ? "100%" : "0")};
+  opacity: ${(props) => (props.clicked ? "1" : "0")};
+
+  transition: width 0.8s, opacity 0.8s;
+`;
+
+const List = styled.ul`
+  position: absolute;
+  list-style: none;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  width: 100%;
+`;
+const ItemLink = styled(NavLink)`
+  display: inline-block;
+  font-size: 3rem;
+  font-weight: 300;
+  text-decoration: none;
+  color: ${COLORS.primaryLight};
+  padding: 1rem 2rem;
+
+  background-image: linear-gradient(
+    120deg,
+    transparent 0%,
+    transparent 50%,
+    #fff 50%
+  );
+  background-size: 240%;
+  transition: all 0.4s;
+
+  &:hover,
+  &:active {
+    background-position: 100%;
+    color: ${COLORS.primaryDark};
+    transform: translateX(1rem);
+  }
+`;
+
+function HamburgerMenu() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  return (
+    <div className="z-10 ">
+      <MenuLabel htmlFor="navi-toggle" onClick={handleClick}>
+        <Icon clicked={click}>&nbsp;</Icon>
+      </MenuLabel>
+      <NavBackground className="h-screen" clicked={click}>&nbsp;</NavBackground>
+
+      <Navigation className="h-screen" clicked={click}>
+        <List className="font-audiowide text-white">
+          <li>
+            <ItemLink onClick={handleClick} to="/">
+              Workshops
+            </ItemLink>
+          </li>
+          <li>
+            <ItemLink onClick={handleClick} to="/about">
+              Schedule
+            </ItemLink>
+          </li>
+          <li>
+            <ItemLink onClick={handleClick} to="/portfolio">
+              Gallery
+            </ItemLink>
+          </li>
+          <li>
+            <ItemLink onClick={handleClick} to="/blog">
+              Sponsers
+            </ItemLink>
+          </li>
+         
+        </List>
+      </Navigation>
+    </div>
+  );
+}
+
+export default HamburgerMenu;
