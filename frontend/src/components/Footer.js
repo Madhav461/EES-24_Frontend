abb7abe7aef47623dc82d83095b950d141b91fc3
Jarvis-013 <124374386+Jarvis-013@users.noreply.github.com>
2023-12-24 22:06:53 +0530

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,12 +3,13 @@ import Icons from "./icons";
 import "./Footer.css";
 import Footerlinks from "./Footerlinks";
 import FooterInputForm from "./FooterInputForm";
-import Triangle from "./triangle";
+
 
 const Footer = () => {
   return (
     <div className="relative  footer-container w-full bottom-0  text-left text-77xl text-pitchdeck-slide-background font-goldman ">
-      <Triangle/>
+      
+      <div class="angle-rectangle"></div>
       <div className="relative  max-w-full max-h-fit  flex flex-col items-end justify-center text-mini font-bruno-ace-sc  ">
         <div
           className="relative overflow-hidden flex flex-col w-[95vw]  h-[33.25rem]   shrink-0 "
@@ -16,31 +17,31 @@ const Footer = () => {
         >
           {/* get in touch heading */}
           <div className=" footer-top-section flex items-center justify-center relative w-full h-fit">
-            <div className="bg-transparent font-goldman text-[48px] outline-none m-[2rem] border-solid border-2  border-white tracking-[0.08em] text-pitchdeck-slide-background text-left flex items-center">
+            <div className="bg-transparent font-goldman text-[350%] outline-none mt-[3rem] border-solid border-2  border-white tracking-[.2rem] text-pitchdeck-slide-background text-left flex items-center p-[0.5rem]">
               GET IN TOUCH
             </div>
 
-            <div className="footer-line w-[60%] h-[30%] "></div>
+            <div className="footer-line w-[60%] h-[30%]  "></div>
           </div>
 
          
 
           <div className="footer-content-container  gap-2  flex relative w-full h-[75%]  justify-between">
-            <div className=" h-full w-[15%] flex items-center">
+            <div className=" h-full w-[15%] flex items-center overflow-hidden">
               <Icons />
             </div>
 
             {/* icons and form separating line ka div */}
-            <div className="h-full w-0.5  bg-pitchdeck-slide-background" />
+            <div className="h-[97%] w-0.5  bg-pitchdeck-slide-background overflow-hidden" />
 
-            <div className="h-full w-[45%] overflow-hidden  object-contain  flex items-center">
+            <div className="h-full w-[45%] overflow-hidden  object-contain  flex items-center forminput">
               <FooterInputForm />
             </div>
 
             {/* icons and form separating line ka div */}
-            <div className="h-full w-0.5 bg-pitchdeck-slide-background" />
+            <div className="h-[97%] w-0.5 bg-pitchdeck-slide-background overflow-hidden line2" />
 
-            <div className="h-full w-[35%] overflow-hidden flex items-center">
+            <div className="h-full w-[35%] overflow-hidden flex items-center overflow-hidden input">
               <Footerlinks></Footerlinks>
             </div>
           </div>
