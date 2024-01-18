fb9fa88ba4f3871045f3b7f602e496e2b202ba23
Madhav461 <ygiridhar.madhav.ece21@itbhu.ac.in>
2024-01-18 19:14:05 +0530

    modified:   frontend/src/components/navhome.js

--- frontend/src/components/navhome.js
+++ frontend/src/components/navhome.js
@@ -24,7 +24,7 @@ const Navhome2 =() =>{
            </div>
         
       
-      <div className="relative bg-gray w-screen  h-[42px] 2k:h-[50px] flex  justice justify-between items-center  overflow-hidden text-center text-base  text-white font-audiowide pb-3 overflow-x-hidden">
+      <div className="relative bg-gray w-screen  h-[800px] 2k:h-[50px] flex  justice justify-between items-center  overflow-hidden text-center text-base  text-white font-audiowide pb-3 overflow-x-hidden">
      
      <div className='flex items-center justify-center'>
       <div className="pb-8 z-50 pl-2 left-[15px] pt-4 lg:hidden 2k:block">
