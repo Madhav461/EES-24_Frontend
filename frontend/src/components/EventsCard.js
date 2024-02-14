8ca0aa5b35156e7e665825badcf7cca87b7f531f
santosh-k22 <kothapalli.santosh.ece22@itbhu.ac.in>
2024-02-14 19:25:16 +0530

    added events pages and changes in sponsors section

--- frontend/src/components/EventsCard.js
+++ frontend/src/components/EventsCard.js
@@ -1,7 +1,15 @@
 import "./EventsCard.css";
-import {useState} from 'react';
+import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 
-const EventsCard = ({name, eventNum, clicked, eventName, eventInfo}) => {
+const EventsCard = ({ name, eventNum, clicked, eventName, eventInfo, eventDescription, eventTitle, eventImg }) => {
+  
+  let navigate = useNavigate();
+  const routeChange = (route) => {
+    let path = `/dashboard/${route}`;
+    navigate(path);
+  }
+  
   const [isActive, setIsActive] = useState(false);
 
   const handleClick = () => {
@@ -12,63 +20,80 @@ const EventsCard = ({name, eventNum, clicked, eventName, eventInfo}) => {
     setIsActive(false);
   }
 
-  const eventClicked=()=>{
+  const eventClicked = () => {
     clicked(name);
     handleClick();
   }
 
-  if(eventName.length == 0){
+  if (eventName.length == 0) {
     eventName = "Event Name";
   }
-  if(eventInfo.length == 0){
+  if (eventInfo.length == 0) {
     eventInfo = "Event Info here. Nothing to add as of now. Hehe !!";
   }
 
   return (
-      <div className="events-card-container">
-        {!isActive && <div className="event-title" onClick={eventClicked}>  
-          {/* <img className="event-title-name" src="events-temporary-name.svg" alt="" /> */}
-          <img className="event-title-name" src="https://imgs.search.brave.com/n15FGBjDSWoR2iVHGR6Qz1xFaXpnP3by76UiRHyVgQI/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuY3RmYXNzZXRz/Lm5ldC9ocmx0eDEy/cGw4aHEvNUtpS21W/RXNDUVBNTnJiT0U2/dzBPdC8zNDFjNTcz/NzUyYmYzNWNiOTY5/ZTIxZmNkMjc5ZDNm/OS9oZXJvLWltZ19j/b3B5LmpwZz9maXQ9/ZmlsbCZ3PTYwMCZo/PTEyMDA" alt="" />
-        </div>}
-        {!isActive && <div className="event-details" onClick={eventClicked}>
+    <div className="events-card-container">
+      {!isActive && <div className="event-title" onClick={eventClicked}>
+        {/* <img className="event-title-name" src="events-temporary-name.svg" alt="" /> */}
+        <img className="event-title-name" src={eventTitle[name-1]} alt="" />
+      </div>}
+      {!isActive && <div className="event-details" onClick={eventClicked}>
 
-          <div className="events-trapezium">
-            {eventName}
-            <span class="before"> </span>
-            <span class="after"> </span>
+        <div className="events-trapezium">
+          {/* {eventName} */}
+          {eventName[name - 1]}
+          <span class="before"> </span>
+          <span class="after"> </span>
+        </div>
+        <div className="event-info-details">
+          {/* {eventInfo} */}
+          <div className="event-info-details-content">
+          {eventInfo[name - 1]}
           </div>
-          <div className="event-info-details">
-            {eventInfo}
-          </div>
-        </div>}
-        {/* <div className="event-register">
+        </div>
+      </div>}
+      {/* <div className="event-register">
           <img src="event-register.svg" alt="" />
               Register
         </div> */}
-        <div className="event-register">
-          <img src="event-register.svg" alt="" />
-          <div className="event-register-title">
-            Register
-          </div>
-        </div>
-        {!isActive && <div className="event-lower-polygon" onClick={eventClicked}>
-          {/* <img src="event-lower-polygon.svg" alt="" /> */}
-        </div>}
 
-        {isActive && <div className="event-pop" onClick={eventClicked}>   
-          <div className="event-info-pop">  
-            Hola I am Santosh
-          </div>
-        </div>}
-        {!isActive && <img className="events-card-background" src="https://imgs.search.brave.com/e6xk7cPNxbtSsfcyr7hDqII9nFQjWBHwxIVRI9hggGg/rs:fit:500:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy82/LzZiL0RlcHRfb2Zf/RWxlY3RyaWNhbF9F/bmdpbmVlcmluZ19J/SVQtQkhVLmpwZw" alt="" 
-        /> }
-          {/* <img className="events-card-background" src="https://imgs.search.brave.com/e6xk7cPNxbtSsfcyr7hDqII9nFQjWBHwxIVRI9hggGg/rs:fit:500:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy82/LzZiL0RlcHRfb2Zf/RWxlY3RyaWNhbF9F/bmdpbmVlcmluZ19J/SVQtQkhVLmpwZw" alt="" />  */}
+        <svg className="event-register-svg" width="auto" height="auto" viewBox="0 0 359 118" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M1.73047 17V0H375.23V116.5H302.73L253.73 67.5H52.2305L1.73047 17Z" fill="white" stroke="white" stroke-width="2" />
+        </svg>
+        <div className="event-register-title" onClick={() => routeChange("")}>
+          Register
+        </div>
+
+      {/* <div className="event-register">
+        <svg className="event-register-svg" width="auto" height="auto" viewBox="0 0 359 118" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M1.73047 17V0H375.23V116.5H302.73L253.73 67.5H52.2305L1.73047 17Z" fill="white" stroke="white" stroke-width="2" />
+        </svg>
+
+        <div className="event-register-title">
+          Register
+        </div>
+      </div> */}
+      {!isActive && <div className="event-lower-polygon" onClick={eventClicked}>
+        {/* <img src="event-lower-polygon.svg" alt="" /> */}
+      </div>}
+
+      {isActive && <div className="event-pop" onClick={eventClicked}>
+        <div className="event-info-pop">
+          {eventDescription[name - 1]}
+        </div>
+      </div>}
+      {!isActive && 
+      // <img className="events-card-background" src="https://imgs.search.brave.com/e6xk7cPNxbtSsfcyr7hDqII9nFQjWBHwxIVRI9hggGg/rs:fit:500:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy82/LzZiL0RlcHRfb2Zf/RWxlY3RyaWNhbF9F/bmdpbmVlcmluZ19J/SVQtQkhVLmpwZw" alt=""/>
+      <img className="events-card-background" src={eventImg[name-1]} alt="" onClick={eventClicked}
+      />}
+      {/* <img className="events-card-background" src="https://imgs.search.brave.com/e6xk7cPNxbtSsfcyr7hDqII9nFQjWBHwxIVRI9hggGg/rs:fit:500:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy82/LzZiL0RlcHRfb2Zf/RWxlY3RyaWNhbF9F/bmdpbmVlcmluZ19J/SVQtQkhVLmpwZw" alt="" />  */}
     </div>
   );
   // return (
   //     <div className="events-card-container">
   //       {!isActive && <div className="event-title" onClick={eventClicked}>  
-        
+
   //       </div>}
   //       {!isActive && <div className="event-details" onClick={eventClicked}>
 
@@ -102,7 +127,7 @@ const EventsCard = ({name, eventNum, clicked, eventName, eventInfo}) => {
   //   <div className="events-card-container-outer">
   //     <div className="events-card-container">
   //       {isActive && <div className="event-title" onClick={eventClicked}>  
-        
+
   //       </div>}
   //       {isActive && <div className="event-details" onClick={eventClicked}>
 
--- frontend/src/components/EventsMashal.js
+++ frontend/src/components/EventsMashal.js
@@ -1,27 +1,43 @@
 import "./Events.css";
+import "./EventsUpcoming.css";
 import EventsCard from "./EventsCard";
 import React, { useState } from "react";
 
+import Background from "./background";
+import Navhome2 from "./navhome";
+import Footer from "./Footer";
+
 const EventsMashal = () => {
 
-  const [ClickedEvent, setClickedEvent] = useState(0);
-  const clicked = (num) => {
-    setClickedEvent(num);
-  }
-
-
   return (
-    <div className="events-container">
-        <EventsCard name="1" eventNum={ClickedEvent} clicked={clicked} eventName="" eventInfo="" eventImg=""/>
-        <EventsCard name="2" eventNum={ClickedEvent} clicked={clicked} eventName="" eventInfo="" eventImg=""/>
-        <EventsCard name="3" eventNum={ClickedEvent} clicked={clicked} eventName="" eventInfo="" eventImg=""/>
-        <EventsCard name="4" eventNum={ClickedEvent} clicked={clicked} eventName="" eventInfo="" eventImg=""/>
-        <EventsCard name="5" eventNum={ClickedEvent} clicked={clicked} eventName="" eventInfo="" eventImg=""/>
-        <EventsCard name="6" eventNum={ClickedEvent} clicked={clicked} eventName="" eventInfo="" eventImg=""/>
-        <EventsCard name="7" eventNum={ClickedEvent} clicked={clicked} eventName="" eventInfo="" eventImg=""/>
-        <EventsCard name="8" eventNum={ClickedEvent} clicked={clicked} eventName="" eventInfo="" eventImg=""/>
-    </div>
+    <>
+      <Background />
+      <Navhome2 />
+        <div className="events-upcoming-container">
+          <img className="events-upcoming-background" src="/events-coming-soon.svg" alt=""></img>
+        </div>
+      {/* <Footer /> */}
+    </>
   );
+
+  // const [ClickedEvent, setClickedEvent] = useState(0);
+  // const clicked = (num) => {
+  //   setClickedEvent(num);
+  // }
+
+
+  // return (
+  //   <div className="events-container">
+  //       <EventsCard name="1" eventNum={ClickedEvent} clicked={clicked} eventName="" eventInfo="" eventImg=""/>
+  //       <EventsCard name="2" eventNum={ClickedEvent} clicked={clicked} eventName="" eventInfo="" eventImg=""/>
+  //       <EventsCard name="3" eventNum={ClickedEvent} clicked={clicked} eventName="" eventInfo="" eventImg=""/>
+  //       <EventsCard name="4" eventNum={ClickedEvent} clicked={clicked} eventName="" eventInfo="" eventImg=""/>
+  //       <EventsCard name="5" eventNum={ClickedEvent} clicked={clicked} eventName="" eventInfo="" eventImg=""/>
+  //       <EventsCard name="6" eventNum={ClickedEvent} clicked={clicked} eventName="" eventInfo="" eventImg=""/>
+  //       <EventsCard name="7" eventNum={ClickedEvent} clicked={clicked} eventName="" eventInfo="" eventImg=""/>
+  //       <EventsCard name="8" eventNum={ClickedEvent} clicked={clicked} eventName="" eventInfo="" eventImg=""/>
+  //   </div>
+  // );
 };
 
 export default EventsMashal;
