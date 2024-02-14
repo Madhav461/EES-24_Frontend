d468e6a0c32c191bff88f3918c2534f1a49f6adb
Madhav461 <ygiridhar.madhav.ece21@itbhu.ac.in>
2024-02-14 16:21:49 +0530

    changes
    events

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ import Signup from "./components/signup";
 
 import Dashboard from "./components/dashboard.js";
 
-import Events from "./components/Events.js";
+// import Events from "./components/Events.js";
 import EventsHome from "./components/EventsHome.js";
 import EventsUdyam from "./components/EventsUdyam.js";
 import EventsUdgam from "./components/EventsUdgam.js";
@@ -35,15 +35,15 @@ function App() {
             <Route exact path="/login" element={<Login />} />
             <Route exact path="/signup" element={<Signup />} />
             <Route exact path="/dashboard" element={<Dashboard />} />
-
+            <Route exact path="/events" element={<EventsHome />} />
             {/* <Route exact path="/events" element={<Events />} /> */}
             
             <Route exact path="/events/udyam" element={<EventsUdyam />} />
             <Route exact path="/events/udgam" element={<EventsUdgam />} />
             <Route exact path="/events/mashal" element={<EventsMashal />} />
-            <Route exact path="/events" element={<EventsHome />} />
-
-//             <Route exact path="/events" element={<Events />} />
+            
+{/* 
+//             <Route exact path="/events" element={<Events />} /> */}
             <Route exact path="/teams" element={<TEAMS_EXTERNAL />} />
 
           </Routes>
